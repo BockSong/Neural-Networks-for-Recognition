ff6e26e0218470c1730c6e6d452a4d98c6b5efd6
BockSong <songrcs@gmail.com>
2020-10-19 16:21:59 -0400

    complete 7.1.4, on debugging

--- python/run_q7_1_4.py
+++ python/run_q7_1_4.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import scipy.io
 import torch
@@ -8,12 +9,23 @@ from torchvision import transforms, datasets, models
 
 import matplotlib.pyplot as plt
 
-max_iters = 50
+import skimage
+import skimage.measure
+import skimage.color
+import skimage.restoration
+import skimage.io
+import skimage.filters
+import skimage.morphology
+import skimage.segmentation
+
+from q4 import *
+
+max_iters = 2
 batch_size = 8
 learning_rate = 1e-3
 input_size = 28 # 28 * 28 = 784
 cnn_layers = [2, 'P']
-linear_layers = [392, 10] # 784 * 2 / (2*2) = 392
+linear_layers = [392, 47] # 784 * 2 / (2*2) = 392
 
 class ConvNet(nn.Module):
     def __init__(self, num_feats, cnn_layers, linear_layers, feat_dim=1):
@@ -50,7 +62,8 @@ if __name__ == '__main__':
     print("IF_CUDA: ", cuda)
 
     # load dataset
-    torchvision.datasets.EMNIST.url = 'http://www.itl.nist.gov/iaui/vip/cs_links/EMNIST/gzip.zip'
+    trans = transforms.ToTensor()
+    datasets.EMNIST.url = 'http://www.itl.nist.gov/iaui/vip/cs_links/EMNIST/gzip.zip'
     train_emnist = datasets.EMNIST(root='../data', split='balanced', train=True, download=True, transform=trans)
     print(train_emnist)
 
@@ -61,11 +74,11 @@ if __name__ == '__main__':
     # define model
     model = ConvNet(input_size, cnn_layers, linear_layers, feat_dim=1)
     print(model)
-
+    '''
+    # Training model
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-    # Training model
     device = torch.device("cuda" if cuda else "cpu")
     model.to(device)
 
@@ -103,22 +116,103 @@ if __name__ == '__main__':
 
         train_accs.append(this_acc)
         train_loss.append(this_loss)
+    
+    torch.save(model.state_dict(),'./q7_1_4_weights.pkl')
+    '''
+    model.load_state_dict(torch.load('./q7_1_4_weights.pkl'))
+
+    # evaluate on findLetters
+    for idx, img in enumerate(os.listdir('../images')):
+        print(img)
+        im1 = skimage.img_as_float(skimage.io.imread(os.path.join('../images',img)))
+        bboxes, bw = findLetters(im1)
+        '''
+        plt.figure(str(idx))
+        plt.imshow(bw, cmap='gray')
+        for bbox in bboxes:
+            minr, minc, maxr, maxc = bbox
+            rect = matplotlib.patches.Rectangle((minc, minr), maxc - minc, maxr - minr,
+                                    fill=False, edgecolor='red', linewidth=2)
+            plt.gca().add_patch(rect)
+        plt.savefig("vis42_" + str(idx))
+        #plt.show()
+        '''
+
+        # find the rows using..RANSAC, counting, clustering, etc.
+        ##########################
+        ##### your code here #####
+        ##########################
+        heights = [b[2] - b[0] for b in bboxes]
+        mean_height = sum(heights) / len(heights)
         
-    # draw plots
-    epochs = list(range(max_iters))
+        # sort
+        bboxes = sorted(bboxes, key=lambda b:b[0])
 
-    plt.figure("Accuracy")
-    plt.title("Acc over epochs")
-    plt.xlabel("Epochs") 
-    plt.ylabel("Accuracy") 
-    plt.plot(epochs, train_accs) 
-    plt.savefig("712Accuracy")
-    #plt.show()
+        # row clustering
+        rows = []
+        row = []
+        line_bottom = bboxes[0][2]
+        for bbox in bboxes:
+            if bbox[0] < line_bottom + mean_height:
+                row.append(bbox)
+            else:
+                rows.append(row)
+                # start new row
+                row = [bbox]
+                line_bottom = bbox[2]
+        rows.append(row)
 
-    plt.figure("Loss")
-    plt.title("Averaged loss over epochs")
-    plt.xlabel("Epochs") 
-    plt.ylabel("Loss") 
-    plt.plot(epochs, train_loss) 
-    plt.savefig("712Loss")
-    #plt.show()
+        # crop the bounding boxes
+        # note.. before you flatten, transpose the image (that's how the dataset is!)
+        # consider doing a square crop, and even using np.pad() to get your images looking more like the dataset
+        ##########################
+        ##### your code here #####
+        ##########################
+        lines = []
+        for row in rows:
+            line = []
+            for bbox in row:
+                minr, minc, maxr, maxc = bbox[0], bbox[1], bbox[2], bbox[3]
+                cropped = bw[minr:maxr, minc:maxc]
+
+                # padding
+                w, h = maxc - minc, maxr - minr
+                if w > h:
+                    w_pad = 0
+                    h_pad = (w - h) // 2
+                else:
+                    h_pad = 0
+                    w_pad = (h - w) // 2
+                p = np.pad(cropped, ((h_pad, h_pad), (w_pad, w_pad)), mode='constant', constant_values=(1, 1))
+                res = skimage.transform.resize(p, (input_size, input_size))
+                trans = np.transpose(res)
+                flat = np.reshape(trans, (1, -1))
+                line.append(flat)
+
+            lines.append(line)
+        
+        # load the weights
+        #import pickle
+        #params = pickle.load(open('q3_weights.pickle','rb'))
+
+        import string
+        idx2str = string.digits + string.ascii_uppercase + "abdefghnqrt"
+        #print(idx2str)
+        text = ""
+
+        # run the crops through your neural network and print them out
+        model.eval()
+        for line in lines:
+            for input in line:
+                #print(input.shape)
+                input = np.reshape(input, (1, 1, input_size, input_size))
+                input = torch.from_numpy(input).type(torch.float32)
+                outputs = model(input)
+                _, pred_labels = torch.max(nn.functional.softmax(outputs, dim=1), 1)
+                pred_labels = pred_labels.view(-1)
+                #print(pred_labels[0])
+        
+                text += idx2str[pred_labels[0]]
+            text += "\n"
+
+        print(text)
