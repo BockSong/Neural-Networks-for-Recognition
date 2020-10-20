c2946573074934029f81c3746496985f58b61c1c
BockSong <songrcs@gmail.com>
2020-10-20 19:57:56 -0400

    complete 5.3.1

--- python/run_q5.py
+++ python/run_q5.py
@@ -4,6 +4,8 @@ from nn import *
 from util import *
 from collections import Counter
 
+import matplotlib.pyplot as plt
+
 train_data = scipy.io.loadmat('../data/nist36_train.mat')
 valid_data = scipy.io.loadmat('../data/nist36_valid.mat')
 
@@ -34,6 +36,8 @@ hidden3_size = 32
 output_size = 1024
 momentum = 0.9
 
+train_data_num = len(train_x)
+
 initialize_weights(input_size, hidden1_size, params, 'layer1')
 initialize_weights(hidden1_size, hidden2_size, params, 'layer2')
 initialize_weights(hidden2_size, hidden3_size, params, 'layer3')
@@ -41,8 +45,11 @@ initialize_weights(hidden3_size, output_size, params, 'output')
 assert(params['Wlayer1'].shape == (input_size, hidden1_size))
 assert(params['blayer1'].shape == (hidden1_size, ))
 
-for key in params.keys():
+keys = [k for k in params.keys()]
+for key in keys:
     params[key + "_m"] = np.zeros_like(params[key])
+'''
+train_loss = []
 
 # should look like your previous training loops
 for itr in range(max_iters):
@@ -74,45 +81,74 @@ for itr in range(max_iters):
         total_loss += loss
 
         # backward
-        delta1 = 2 * (xb - pred)
+        delta1 = 2 * (pred - xb)
         delta2 = backwards(delta1,params,'output',sigmoid_deriv)
         delta3 = backwards(delta2,params,'layer3',relu_deriv)
         delta4 = backwards(delta3,params,'layer2',relu_deriv)
         delta5 = backwards(delta4,params,'layer1',relu_deriv)
 
         # apply gradient
-        params['W' + "output" + "_m"] = params['W' + "output" + "_m"] * momentum - learning_rate * params['grad_W' + "output"]
-        params['b' + "output" + "_m"] = params['b' + "output" + "_m"] * momentum - learning_rate * params['grad_b' + "output"]
-        params['W' + "output"] += params['W' + "output" + "_m"]
-        params['b' + "output"] += params['b' + "output" + "_m"]
+        layer_name = ["output", "layer3", "layer2", "layer1"]
+        for name in layer_name:
+            params['W' + name + "_m"] = params['W' + name + "_m"] * momentum - learning_rate * params['grad_W' + name]
+            params['b' + name + "_m"] = params['b' + name + "_m"] * momentum - learning_rate * params['grad_b' + name]
+            params['W' + name] += params['W' + name + "_m"]
+            params['b' + name] += params['b' + name + "_m"]
 
-        params['W' + "layer3" + "_m"] = params['W' + "layer3" + "_M"] * momentum - learning_rate * params['grad_W' + "layer3"]
-        params['b' + "layer3" + "_m"] = params['b' + "layer3" + "_M"] * momentum - learning_rate * params['grad_b' + "layer3"]
-        params['W' + "layer3"] += params['W' + "layer3" + "_M"]
-        params['b' + "layer3"] += params['b' + "layer3" + "_M"]
-
-        params['W' + "layer2" + "_m"] = params['W' + "layer2" + "_m"] * momentum - learning_rate * params['grad_W' + "layer2"]
-        params['b' + "layer2" + "_m"] = params['b' + "layer2" + "_m"] * momentum - learning_rate * params['grad_b' + "layer2"]
-        params['W' + "layer2"] += params['W' + "layer2" + "_M"]
-        params['b' + "layer2"] += params['b' + "layer2" + "_M"]
-
-        params['W' + "layer1" + "_m"] = params['W' + "layer1" + "_m"] * momentum - learning_rate * params['grad_W' + "layer1"]
-        params['b' + "layer1" + "_m"] = params['b' + "layer1" + "_m"] * momentum - learning_rate * params['grad_b' + "layer1"]
-        params['W' + "layer1"] += params['W' + "layer1" + "_M"]
-        params['b' + "layer1"] += params['b' + "layer1" + "_M"]
+    total_loss /= train_data_num
+    train_loss.append(total_loss)
 
     if itr % 2 == 0:
         print("itr: {:02d} \t loss: {:.2f}".format(itr,total_loss))
     if itr % lr_rate == lr_rate-1:
         learning_rate *= 0.9
-        
+
+# draw plots
+epochs = list(range(max_iters))
+
+plt.figure("Loss")
+plt.title("Averaged loss over epochs")
+plt.xlabel("Epochs") 
+plt.ylabel("Loss") 
+plt.plot(epochs, train_loss) 
+plt.savefig("52Loss")
+#plt.show()
+'''
+
+import pickle
+'''
+saved_params = {k:v for k,v in params.items() if '_' not in k}
+with open('q5_weights.pickle', 'wb') as handle:
+    pickle.dump(saved_params, handle, protocol=pickle.HIGHEST_PROTOCOL)
+'''
+params = pickle.load(open('q5_weights.pickle','rb'))
+
 # Q5.3.1
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 # visualize some results
 ##########################
 ##### your code here #####
 ##########################
+for c in range(5):
+    for n in range(2):
+        input = valid_x[c*100+n].reshape((1, -1))
 
+        img = input.reshape(32,32).T
+        plt.figure(c*100+n)
+        plt.imshow(img)
+        plt.savefig("531_" + str(c) + str(n))
+        #plt.show()
+
+        h1 = forward(input, params, 'layer1', relu)
+        h2 = forward(h1, params, 'layer2', relu)
+        h3 = forward(h2, params, 'layer3', relu)
+        rec = forward(h3, params, 'output', sigmoid)
+
+        img_rec = rec.reshape(32,32).T
+        plt.figure(c*100+n+50)
+        plt.imshow(img_rec)
+        plt.savefig("531_" + str(c) + str(n) + "_rec")
+        #plt.show()
 
 # Q5.3.2
 from skimage.measure import compare_psnr as psnr
